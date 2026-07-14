3d4ca9c87725e615a8999d5cbe200b7e3a1aa85f
HassanIQ777 <hassaniq793@gmail.com>
2026-07-14 23:50:58 +0300

    added Makefile

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include "libutils/libutils.hpp"
-#include "libutils/src/numutils.hpp"
 
-using namespace std;
-
-const string VERSION = "0.4";
+const std::string VERSION = "0.4";
 
 int main(int argc, char *argv[]) {
   CLIParser parser(argc, argv);
@@ -16,20 +13,21 @@ int main(int argc, char *argv[]) {
   }
 
   if (parser.hasFlag("-v")) {
-    cout << "sizefile version " << VERSION << endl;
+    std::cout << "sizefile version " << VERSION << std::endl;
     return 0;
   }
 
   for (int i = 1; i < argc; i++) {
-    string filename = parser.getArg(i);
+    std::string filename = parser.getArg(i);
     if (!File::isfile(filename)) {
       //   Log::warn("ERROR: the provided path isn't a file");
       continue;
     }
 
     float filesize = File::getfilesize(filename); // bytes
-    string filesize_with_unit = numutils::bytes(filesize);
+    std::string filesize_with_unit = numutils::bytes(filesize);
 
-    cout << "size of '" << filename << "' is " << filesize_with_unit << endl;
+    std::cout << "size of '" << filename << "' is " << filesize_with_unit
+              << std::endl;
   }
 }
